b4fb1293c9aa913da88c06b21d977ef55a3aa7f4
Cyril Lazar <klazar@mail.ru>
2023-02-27 15:31:28 +1000

    feat: сделал футер

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,29 @@
+import { Link } from 'react-router-dom';
+import './Footer.css';
+
 function Footer() {
-    return (
-        <footer className="Footer">
-            <h2>footer</h2>
-        </footer>
-    )
-};
+  return (
+    <footer className="footer centred-block">
+      <p className="footer__head">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </p>
+      <div className="footer__container">
+        <p className="footer__copy">&copy; 2020</p>
+        <ul className="footer__list">
+          <li className="footer__item">
+            <Link className="footer__link" to={'*'}>
+              Яндекс.Практикум
+            </Link>
+          </li>
+          <li className="footer__item">
+            <Link className="footer__link" to={'*'}>
+              Github
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
 
 export default Footer;
