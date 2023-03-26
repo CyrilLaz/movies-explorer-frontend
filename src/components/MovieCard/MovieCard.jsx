3b2c8c523d5529b60490bc9b8b16c4b025e454cb
Cyril Lazar <klazar@mail.ru>
2023-03-26 23:29:35 +1000

    feat: формат-ние строки длительности фильмов

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -9,10 +9,22 @@ function MovieCard(props) {
       : ' movies-card__button_type_like'
   }`;
 
+  const formatDuration =
+    `${
+      Math.floor(props.duration / 60) !== 0
+        ? `${Math.floor(props.duration / 60)}ч `
+        : ''
+    }` +
+    (props.duration % 60) +
+    'м';
   return (
     <li className="movies-card">
       <figure className="movies-card__container">
-        <Link className="movies-card__link" to={props.trailerLink} target='_blank'>
+        <Link
+          className="movies-card__link"
+          to={props.trailerLink}
+          target="_blank"
+        >
           <img
             className="movies-card__picture"
             src={props.image}
@@ -22,11 +34,11 @@ function MovieCard(props) {
         <figcaption className="movies-card__capture">
           <div className="movies-card__description">
             <h2 className="movies-card__title">{props.nameRU}</h2>
-            <span className="movies-card__duration">{props.duration}</span>
+            <span className="movies-card__duration">{formatDuration}</span>
           </div>
           <button
             className={cardButtonClassName}
-            onClick={props.isLiked?props.deleteLike:props.putLike}
+            onClick={props.isLiked ? props.deleteLike : props.putLike}
           ></button>
         </figcaption>
       </figure>
